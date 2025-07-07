3964a00de19a451a8823cdc99b59755e7062109e
subbni <slfkak@naver.com>
2025-07-07 13:38:35 +0900

    [init] Initialize project

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(color: Color(0xFF232B55)),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    );
+  }
+}
